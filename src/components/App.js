f0e62f4a7cc48e4daa5ba1497c0c3df373802815
Juggernau9 <xbhavyatomar@gmail.com>
2020-08-20 03:56:48 +0530

    updated logos, meta tags, add og image using react-helmet

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import React from 'react'
 import Navbar from './Navbar'
 import Form from './Form'
 import JokesList from './JokesList'
+import {Helmet} from 'react-helmet'
+import logo from '../images/icon.jpg'
 
 const App = () => {
     return(
         <div>
+            <Helmet>
+                <meta property="og:image" content={logo} />
+            </Helmet>
             <Navbar />
             <div className="container">
                 <div className="row">
