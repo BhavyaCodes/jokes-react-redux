fd06003f5526a84d3d076b6d7d062ccf7a2e903c
Juggernau9 <xbhavyatomar@gmail.com>
2020-08-20 15:32:26 +0530

    update navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import logo from '../images/logo512.png'
+import './Navbar.css'
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="/">Jokes react app</a>
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="/"><img className="nav-logo" alt={logo} src={logo}/></a>
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">Jokes<span className="sr-only">(current)</span></a>
+                </li>
+            </ul>
         </nav>
     )
 }
